162f5bc67f5ea0cf5c846e824de1ce0430afb558
Alan <alanhp2@hotmail.com>
2021-05-05 19:47:35 -0700

    implemented getChart() innto searchContent

--- ClientApp/src/app/searchcontent/searchcontent.component.ts
+++ ClientApp/src/app/searchcontent/searchcontent.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from "../shared.service";
+import { StocksAPIService } from '../stocks-api.service';
 
 @Component({
   selector: 'app-searchcontent',
@@ -9,10 +10,37 @@ import { SharedService } from "../shared.service";
 export class SearchcontentComponent implements OnInit {
   receivedMessage: string;
 
-  constructor(private sharedInput: SharedService) {}
+  constructor(
+    private sharedInput: SharedService,
+    private sData: StocksAPIService,
+  ) { }
 
   ngOnInit() {
-    this.receivedMessage=this.sharedInput.getMessage();
+    this.receivedMessage = this.sharedInput.getMessage();
+    this.sData.getChart()
+      .then((response) => {
+        response.json()
+          .then((data) => {
+            //this.charts = data.chart.result;
+            let high = data.chart.result[0].indicators.quote[0].high;
+            let low = data.chart.result[0].indicators.quote[0].low;
+            let timeStamp = data.chart.result[0].timestamp;
+            let stockDates = [];
+            timeStamp.forEach((data) => {
+              let jsdate = new Date(data * 1000);
+              stockDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric' }));
+            });
+
+            //console.log(this.charts);
+            console.log(high);
+            console.log(low);
+            console.log(timeStamp);
+            console.log(stockDates);
+          });
+      })
+      .catch((err) => {
+        console.log('Error geenrted: ${err}');
+      });
   }
 
 }
--- ClientApp/src/app/stocks-api.service.ts
+++ ClientApp/src/app/stocks-api.service.ts
@@ -28,4 +28,14 @@ export class StocksAPIService {
       }
     });
   }
+
+  getChart() {
+    return fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-chart?interval=5m&symbol=MSFT&range=1d&region=US", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "53a3423ce6mshd2b7f90067c439dp1d6a06jsn1e55b49a3aef",
+        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+      }
+    });
+  }
 }
