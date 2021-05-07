95b5b544ccd3c9822051576c4f5c7664badba01a
Alan <alanhp2@hotmail.com>
2021-05-06 21:59:21 -0700

    added button press when user pressed enter

--- ClientApp/src/app/searchbar/searchbar.component.ts
+++ ClientApp/src/app/searchbar/searchbar.component.ts
@@ -5,13 +5,22 @@ import { SharedService } from "../shared.service";
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
   styleUrls: ['./searchbar.component.css']
-})
+}) 
 export class SearchbarComponent implements OnInit {
-  currentVal = "";
 
   constructor(private sharedInput: SharedService) { }
 
   ngOnInit() {
+    const input = document.querySelector(".search");
+    input.addEventListener("keyup", function (event: KeyboardEvent) {
+      // Number 13 is the "Enter" key on the keyboard
+      if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("myBtn").click();
+      }
+    });
   }
 
   setVal(val) {
